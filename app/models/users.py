ad171db9c87c4485ea98c1e54727b2a804f0e71f
michael7nightingale <suslanchikmopl@gmail.com>
2023-10-03 17:34:21 +0500

    create models: user, teacher, pupil, administrator

---
+++ app/models/users.py
@@ -0,0 +1,140 @@
+from tortoise import fields
+from tortoise.exceptions import IntegrityError
+from tortoise.transactions import in_transaction
+
+from uuid import uuid4
+from enum import StrEnum, auto
+import random
+from typing import Optional
+
+from .base import TortoiseModel
+from app.services.passwords import verify_password, hash_password
+
+
+def generate_identifier() -> str:
+    return "".join(str(random.randint(0, 9)) for _ in range(8))
+
+
+def generate_password() -> str:
+    psw = str(uuid4()).replace("-", "")
+    idx_range = list(range(len(psw)))
+    random.shuffle(idx_range)
+    return "".join(psw[idx] for idx in idx_range)
+
+
+class UserRole(TortoiseModel):
+    name = fields.CharField(unique=True, max_length=100)
+
+
+class User(TortoiseModel):
+    id = fields.CharField(pk=True, default=generate_identifier, max_length=18)
+    first_name = fields.CharField(max_length=100)
+    last_name = fields.CharField(max_length=100)
+    father_name = fields.CharField(max_length=100)
+    email = fields.CharField(unique=True, index=True, max_length=100)
+    password = fields.CharField(max_length=255, default=generate_password)
+    username = fields.CharField(max_length=255, unique=True, index=True)
+    type = fields.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.father_name} {self.first_name} {self.father_name}"
+
+    @classmethod
+    async def login(cls, password: str, email: str | None = None, username: str | None = None) -> Optional["User"]:
+        if email is None and username is None:
+            return None
+        if email is not None and username is not None:
+            return None
+        statements = {"email": email} if email is not None else {"username": username}
+        user = await cls.get_or_none(**statements)
+        if user is not None:
+            if verify_password(password, user.password):
+                return user
+        return None
+
+    @classmethod
+    async def register(
+            cls,
+            type: str,
+            first_name: str,
+            last_name: str,
+            father_name: str,
+            username: str,
+            email: str,
+            password: str | None = None,
+            **kwargs
+    ) -> Optional["User"]:
+        if password is None:
+            password = generate_password()
+        try:
+            new_user = await cls.create(
+                first_name=first_name,
+                last_name=last_name,
+                father_name=father_name,
+                username=username,
+                email=email,
+                password=hash_password(password),
+                type=type
+            )
+            return new_user
+        except IntegrityError:
+            return None
+
+
+class UserTypeModel(TortoiseModel):
+    type: str
+    user = fields.OneToOneField("models.User")
+
+    @classmethod
+    async def create(cls, **kwargs) -> Optional["UserTypeModel"]:
+        async with in_transaction():
+            user = await User.register(type=cls.type, **kwargs)
+            if user is None:
+                return
+            return await super().create(**kwargs, user=user)    # type: ignore
+
+    class Meta:
+        abstract = True
+
+
+class Teacher(UserTypeModel):
+    type = "teacher"
+    user = fields.OneToOneField("models.User", "teacher")
+
+
+class Pupil(UserTypeModel):
+    type = "pupil"
+    user = fields.OneToOneField("models.User", "pupil")
+
+
+class UserTypesEnum(StrEnum):
+    teacher = auto()
+    pupil = auto()
+    administrator = auto()
+
+
+class Administrator(UserTypeModel):
+    type = "administrator"
+    user = fields.OneToOneField("models.User", "administrator")
+
+
+user_types_matching = {
+    Pupil.type: Pupil,
+    Teacher.type: Teacher,
+    Administrator.type: Administrator,
+
+}
+
+
+async def get_typed_user(type: str, user_id: str) -> UserTypeModel | None:
+    model_class = user_types_matching.get(type)
+    if model_class is None:
+        return
+    return await model_class.get_or_none(user_id=user_id)
+
+
+async def create_typed_user(type: str, **kwargs) -> Optional[UserTypeModel]:
+    model_class = user_types_matching.get(type)
+    if model_class is None:
+        return
+    return await model_class.create(**kwargs)
