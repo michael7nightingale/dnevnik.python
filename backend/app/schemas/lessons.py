29774ff466afba694b2c79e7f1d75d05cf3e69f5
michael7nightingale <suslanchikmopl@gmail.com>
2023-10-08 11:10:30 +0500

    change lessons schemas depending on db models structure

--- backend/app/schemas/lessons.py
+++ backend/app/schemas/lessons.py
@@ -1,13 +1,39 @@
 from pydantic import BaseModel
 import datetime
 
+from app.schemas.classes import StudyGroupScheme
 from app.schemas.users import TeacherScheme
 
 
+class BaseSubjectScheme(BaseModel):
+    name: str
+
+
+class SubjectCreateScheme(BaseSubjectScheme):
+    pass
+
+
+class SubjectScheme(BaseSubjectScheme):
+    id: str
+
+
+class BaseStudyGroupSubjectScheme(BaseModel):
+    subject: SubjectScheme
+    study_group_id: str
+    teacher: TeacherScheme
+
+
+class StudyGroupSubjectScheme(BaseStudyGroupSubjectScheme):
+    id: str
+
+
 class BaseLessonScheme(BaseModel):
+    start_at: datetime.time | None = None
     date: datetime.date | None = None
-    science: str
+    finish_at: datetime.time | None = None
     theme: str
+    homework: str | None = None
+    study_group_subject_id: str
 
 
 class LessonScheme(BaseLessonScheme):
@@ -15,11 +41,11 @@ class LessonScheme(BaseLessonScheme):
 
 
 class LessonFullScheme(BaseLessonScheme):
-    start_at: datetime.time | None = None
-    finish_at: datetime.time | None = None
+    study_group_subject: StudyGroupSubjectScheme
+
+
+class LessonCreateScheme(BaseLessonScheme):
     study_group_id: str
-    teacher: TeacherScheme
-    homework: str | None = None
 
 
 class LessonUpdateScheme(BaseLessonScheme):
